5babb5fcef303ca9f6c0fa3af60b9fb7376e0b85
ellafink <143015004+ellafink@users.noreply.github.com>
2024-01-16 10:12:36 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -9,7 +9,13 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(){
+		if(num<2)
+    			return false;
+  		for(int i = 2; i<=Math.sqrt(num); i++){
+   			 if(num%i == 0)
+     				 return false;
+  		}
+ 		 return true;
 	}
-
 }
 
